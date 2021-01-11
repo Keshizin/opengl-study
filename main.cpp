a23e3cb780711d61bc164aed42dd05765671de95
Keshizin <fab.takeshi@gmail.com>
2021-01-10 23:38:43 -0300

    Studyng translate and scale transforms

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <GL/glu.h>
 
 #include <ge.h>
+#include <gemath.h>
+
 #include <iostream>
 
 #define GAME_WINDOW_WIDTH 640
@@ -32,11 +34,13 @@ class GLEventHandler : public GEEventHandler
 };
 
 KEngine *engine = 0;
+GETimer *timer = 0;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	GLEventHandler gameEvents;
 	engine = new KEngine(&gameEvents);
+	timer = new GETimer(engine->getTimeHandler());
 
 	// Setting up Game Window
 	engine->getGameWindow()->setName("K-Engine!");
@@ -59,16 +63,80 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	engine->getGameWindow()->show(nCmdShow);
 
+	timer->setTimerInMs(1000);
 	engine->startMainLoop();
 
 	delete engine;
+	delete timer;
 	return 1;
 }
 
+GLfloat tx = 0.0f;
+GLfloat ty = 0.0f;
+GLfloat tz = 0.0f;
+
+GLfloat sx = 1.0f;
+GLfloat sy = 1.0f;
+GLfloat sz = 1.0f;
+
+GLfloat offset_x = 0.05f;
+GLfloat offset_y = 0.05f;
+
+GLfloat angleRad = 0.0f;
+int points = 0;
+
 void GLEventHandler::frameEvent()
 {
+	// ------------------------------------------------------------------------
+	// SCALE TEST
+	// ------------------------------------------------------------------------
+	// sx += offset_x;
+	// sy += offset_y;
+
+	// if(sy > 2.0f || sy < 1.0f)
+	// 	offset_y = -offset_y;
+
+	// if(sy > 2.0f || sy < 1.0f)
+	// 	offset_x = -offset_x;
+
+	// ------------------------------------------------------------------------
+	// TRANSLATE TEST
+	// ------------------------------------------------------------------------
+	if(timer->isDoneLoop())
+	{
+		tx = 25 * cos(angleRad);
+		ty = 25 * sin(angleRad);
+
+		points++;
+		angleRad = 2 * K_PI * points / 200;
+
+		if(points >= 200)
+		{
+			points = 0;
+		}
+
+		std::cout << "tx: " << tx << std::endl;
+		std::cout << "ty: " << ty << std::endl << std::endl;
+
+	}
+
 	glClear(GL_COLOR_BUFFER_BIT);
-	glFlush();
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glTranslatef(tx, ty, tz);
+	// glTranslatef(1.0f, 0.0f, 0.0f);
+	// glScalef(sx, sy, sz);
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glBegin(GL_QUADS);
+	glVertex3f(-20.5f,  20.5f, 0.0f);
+	glVertex3f( 20.5f,  20.5f, 0.0f);
+	glVertex3f( 20.5f, -20.5f, 0.0f);
+	glVertex3f(-20.5f, -20.5f, 0.0f);
+	glEnd();
+
 }
 
 void GLEventHandler::mouseEvent(int button, int state, int x, int y)
@@ -84,6 +152,11 @@ void GLEventHandler::keyboardEvent(unsigned char key, int state)
 	if(key == 27 && state == 1)
 	{
 		engine->getGameWindow()->destroy();
+	}
+
+	if(key == '1' && state == 1)
+	{
+		timer->startLoop(0);
 	}
 }
 
